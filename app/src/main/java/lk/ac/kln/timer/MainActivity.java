2c6bbc2335a4c8a59d4a87b49fd8eb238f607390
umaa1 <umangahasini@gmail.com>
2019-03-13 13:30:53 +0530

    CountDownTimer day3

---
+++ app/src/main/java/lk/ac/kln/timer/MainActivity.java
@@ -0,0 +1,67 @@
+package lk.ac.kln.timer;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private  int counter=100;
+    private  boolean running;
+    private boolean wasRunning;
+    private  static  final String CURRENT_COUNTER="counter";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null){
+            counter=savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+        running=true;
+
+        countDown();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,counter);
+
+    }
+
+
+    private  void countDown(){
+
+        final TextView textView=findViewById(R.id.textView);
+        // textView.setText("hello");
+
+        final Handler handler=new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //reset the counter
+                textView.setText(Integer.toString(counter));
+                if(counter==0){
+                    counter=99;
+                }
+
+                counter--;
+                handler.postDelayed(this,1000);
+            }
+        });
+
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        wasRunning=true;
+    }
+}
+
